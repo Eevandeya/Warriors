a9adf4eaabe82e883567859ef3396bac146ee524
Eevandeya <ivansotenko8@gmail.com>
2022-08-18 22:00:53 +0300

    Оно стреляет!

--- game.py
+++ game.py
@@ -6,19 +6,44 @@ class Warrior1(pygame.sprite.Sprite):
         self.image = pygame.image.load('warrior_1.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=(400, 200))
 
-    def warrior_move(self):
+        self.reload = 0
+
+    def warrior_control(self):
         keys = pygame.key.get_pressed()
+
         if keys[pygame.K_UP]:
-            self.rect.y -= 1
+            self.rect.y -= 2
         if keys[pygame.K_DOWN]:
-            self.rect.y += 1
+            self.rect.y += 2
         if keys[pygame.K_LEFT]:
-            self.rect.x -= 1
+            self.rect.x -= 2
         if keys[pygame.K_RIGHT]:
-            self.rect.x += 1
+            self.rect.x += 2
+
+        if keys[pygame.K_SPACE] and not(self.reload):
+            bullets_group.add(Bullet(self.rect.x, self.rect.y))
+            self.reload = 10
 
     def update(self):
-        self.warrior_move()
+        self.warrior_control()
+
+        if self.reload:
+            self.reload -= 1
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, warrior_x, warrior_y):
+        super().__init__()
+        self.image = pygame.image.load('bullet_2.png').convert_alpha()
+        self.rect = self.image.get_rect(topleft=(warrior_x + 6, warrior_y))
+
+
+    def update(self):
+        self.rect.y -= 4
+
+    def destroy(self):
+        if self.rect.y <= -30:
+            self.kill()
+
 
 pygame.init()
 
@@ -37,6 +62,8 @@ screen.fill('#FFFFFF')
 w1 = pygame.sprite.GroupSingle()
 w1.add(Warrior1())
 
+bullets_group = pygame.sprite.Group()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,6 +71,10 @@ while True:
             exit()
 
     screen.fill('#FFFFFF')
+
+    bullets_group.draw(screen)
+    bullets_group.update()
+
     w1.draw(screen)
     w1.update()
 
