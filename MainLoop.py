157c2372a00e9ed63605b9084e6768f4df6f823d
Eevandeya <ivansotenko8@gmail.com>
2022-12-31 17:07:37 +0300

    Документация MainLoop

--- MainLoop.py
+++ MainLoop.py
@@ -29,69 +29,79 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
+# Создание группы красного игрока
 red_warrior_group = pygame.sprite.GroupSingle()
 red_warrior_group.add(Warrior('bottom'))
 
+# Создание группы синего игрока
 blue_warrior_group = pygame.sprite.GroupSingle()
 blue_warrior_group.add(Warrior('top'))
 
+# Создание групп синих и красных пуль и списка взрывов
 red_bullets_group = pygame.sprite.Group()
 blue_bullets_group = pygame.sprite.Group()
-explosions = []
 
+# Вспомогательные изменяемые параметры
 game_stage = 'battle'
 endscreen_delay = Constants.ENDSCREEN_DELAY
+explosions = []
 winner = None
-
 play_win_sound = True
 
 while True:
+    # Закрытие игры при нажатии крестика
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
     screen.fill('#dcdcdc')
-
     screen.blit(battlefield, (0, 150))
 
+    # Отрисовка и обновление пуль
     red_bullets_group.draw(screen)
     red_bullets_group.update()
-
     blue_bullets_group.draw(screen)
     blue_bullets_group.update()
 
+    # Обновление игроков
     red_warrior_group.update()
     blue_warrior_group.update()
 
+    # Получение списков пуль, которые попали
     red_hit_bullets = pygame.sprite.spritecollide(blue_warrior_group.sprite, red_bullets_group, False)
     blue_hit_bullets = pygame.sprite.spritecollide(red_warrior_group.sprite, blue_bullets_group, False)
 
     if game_stage == 'battle':
 
+        # Обработка выстрелов, если они есть
         blue_shot = blue_warrior_group.sprite.warrior_shots()
+        red_shot = red_warrior_group.sprite.warrior_shots()
+
         if blue_shot != False:
             blue_bullets_group.add(BlueBullet(blue_shot))
-
-        red_shot = red_warrior_group.sprite.warrior_shots()
         if red_shot != False:
             red_bullets_group.add(RedBullet(red_shot))
 
+        # Если какие-то пули попали, уничтожение их и спавн взрывов
         if red_hit_bullets:
             kill_bullet_and_spawn_explosion()
 
         if blue_hit_bullets:
             kill_bullet_and_spawn_explosion()
 
+        # Отрисовка игроков
         red_warrior_group.draw(screen)
         blue_warrior_group.draw(screen)
 
+        # Начисление повреждений, если пули попали
         if red_hit_bullets:
             blue_warrior_group.sprite.do_damage(red_hit_bullets)
 
         if blue_hit_bullets:
             red_warrior_group.sprite.do_damage(blue_hit_bullets)
 
+        # Обработка смертей игроков
         if blue_warrior_group.sprite.heals == 0:
             game_stage = 'end'
             winner = 'red'
@@ -107,6 +117,9 @@ while True:
                                    red_warrior_group.sprite.rect.centery)
 
     elif game_stage == 'end':
+
+        # В двух ветвях происходит отрисовка победителя, обработка пуль противников, определение эндскрина
+        # Обработка выстрелов победителя
         if winner == 'red':
             red_warrior_group.draw(screen)
             if blue_hit_bullets:
@@ -127,6 +140,7 @@ while True:
             if blue_shot != False:
                 blue_bullets_group.add(BlueBullet(blue_shot))
 
+        # После победы, нужно выждать задержку, после появляется эндскрин, играется звук
         if not endscreen_delay:
 
             if play_win_sound:
@@ -137,6 +151,7 @@ while True:
         else:
             endscreen_delay -= 1
 
+    # Обработка всех взрывов
     if explosions:
         update_explosions()
 
